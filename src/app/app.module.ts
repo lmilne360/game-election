24467606b9847a336189923a1b92e249f10cf106
Laurent <lmilne360@gmail.com>
2018-11-25 02:26:41 -0600

    created game election main component & directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameElectionComponent } from './game-election/game-election.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameElectionComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/game-election/game-election.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'game-election',
+  templateUrl: './game-election.component.html',
+  styleUrls: ['./game-election.component.scss']
+})
+export class GameElectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
