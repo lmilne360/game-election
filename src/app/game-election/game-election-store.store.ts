cd4c9b7b9e8ae8da346cefa6b0e0c3f48842f0b5
Laurent <lmilne360@gmail.com>
2018-11-25 04:13:23 -0600

    created methods for voting on & adding a candidate

--- src/app/game-election/game-election-store.store.ts
+++ src/app/game-election/game-election-store.store.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Store } from '../core/store';
 import { GameElectionState } from './game-election-state';
+import { Candidate } from '../core/candidate';
 
 @Injectable()
 export class GameElectionStore extends Store<GameElectionState> {
@@ -8,5 +9,24 @@ export class GameElectionStore extends Store<GameElectionState> {
   constructor() {
     // initializes new state when creating a new instance
     super(new GameElectionState());
+  }
+
+  // loops though candidate objects in the state and increment the vote on the matching candidate
+  addVote(candidate: Candidate): void{
+    this.setState({...this.state, candidates: this.state.candidates.map( c => {
+      if (c === candidate ) {
+        return {...c, votes: c.votes + 1};
+      }
+      return c
+      })
+    });
+  }
+
+  // creates a new candidate from the provided name and adds to state object
+  addCandidate(name: string): void {
+    this.setState({
+      ...this.state,
+      candidates: [...this.state.candidates, {name: name, votes: 0}]
+    });
   }
 }
