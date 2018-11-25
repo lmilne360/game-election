7f973fd104931f164a77e74d72a69fa1b3f68bdc
Laurent <lmilne360@gmail.com>
2018-11-25 05:07:19 -0600

    added  view for candidates

--- src/app/game-election/game-election-state.ts
+++ src/app/game-election/game-election-state.ts
@@ -1,5 +1,13 @@
 import { Candidate } from "../core/candidate";
 
+const CANDIDATES = [
+  {name: 'Mario Kart', votes: 5},
+  {name: 'Dark Souls', votes: 4},
+  {name: 'Soul Calibur', votes: 3},
+  {name: 'OverWatch', votes: 2},
+  {name: 'Minecraft', votes: 1},
+];
+
 export class GameElectionState {
-  candidates: Candidate[] = [];
+  candidates: Candidate[] = CANDIDATES;
 }
--- src/app/game-election/game-election.component.ts
+++ src/app/game-election/game-election.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { GameElectionStore } from './game-election-store.store';
+import { map, distinctUntilChanged } from 'rxjs/operators'
 
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'game-election',
   templateUrl: './game-election.component.html',
-  styleUrls: ['./game-election.component.scss']
+  styleUrls: ['./game-election.component.scss'],
+  providers: [GameElectionStore]
 })
 export class GameElectionComponent implements OnInit {
 
-  constructor() { }
+  constructor(public store: GameElectionStore) { }
 
   ngOnInit() {
+    this.store.state$
+    .pipe(map(state => state.candidates),distinctUntilChanged()) // Subscribes to the updates on the candidates subset of the state
   }
 
 }
